c28042bf6421f71bbfe82e1147740354a488f3ec
Sim38 <simjunhong38@gmail.com>
2025-04-14 18:20:27 +0800

    Set up for gh-pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  //   index("routes/home.tsx"),
+  ...prefix("/portfolio", [
+    route("/", "routes/home.tsx"),
+    route("/about", "routes/about.tsx"),
+  ]),
+] satisfies RouteConfig;
