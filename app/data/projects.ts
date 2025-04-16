cea6e6e7f1d6f30a14f14f32bfc0e0b6d14233d6
Sim38 <simjunhong38@gmail.com>
2025-04-16 14:59:00 +0800

    Added Project Page

---
+++ app/data/projects.ts
@@ -0,0 +1,37 @@
+import streamElement from "public/assets/projects/stream-element.jpg";
+import subathonTimer from "public/assets/projects/subgoal.png";
+import electron from "public/assets/projects/electron-icon.svg";
+import electronApp from "public/assets/projects/electron-app.png";
+import myBusz from "public/assets/projects/mybusz.png";
+import birdFlappy from "public/assets/projects/bird-flappy.png";
+
+export const projects: Array<Project> = [
+  {
+    name: "Stream Tools Application",
+    description:
+      "Software Application used to manage streamer's twitch account, along with their other stream tools, and stream widgets.",
+    tags: ["Electron", "NodeJS", "React"],
+    imageSrc: [electron],
+  },
+  {
+    name: "Stream Widgets",
+    description:
+      "Streaming Widgets used by streamers to enhance their streams. Which include Chatbots, Subathon timers, Stream Scheduler and Twitch Extensions.",
+    tags: ["NodeJS", "React"],
+    imageSrc: [streamElement],
+  },
+  {
+    name: "MyBusz",
+    description:
+      "Mobile Bus Application to check Bus Schedules, Location and Estimated Arrival Time in Malaysia.",
+    tags: ["React Native", "Laravel", "SQL"],
+    imageSrc: [myBusz],
+  },
+  {
+    name: "Bird Flappy",
+    description:
+      "Mobile Game inspired by Flappy Bird, but instead of tapping to move the bird, we tap to move the pipes.",
+    tags: ["Unity", "C#"],
+    imageSrc: [birdFlappy],
+  },
+];
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,3 +1,90 @@
+import { useEffect, useState } from "react";
+import { projects } from "../data/projects";
+import missingImage from "public/assets/missing_image.jpg";
+
 export default function Projects() {
-  return <>Projects</>;
+  return (
+    <div className="flex flex-col w-full h-full items-center">
+      <h1 className="text-4xl">Projects</h1>
+      <ProjectSection />
+    </div>
+  );
 }
+
+const ProjectSection = () => {
+  return (
+    <div>
+      {projects.map((project) => (
+        <ProjectContainer key={project.name} project={project} />
+      ))}
+    </div>
+  );
+};
+
+const ProjectContainer = ({ project }: { project: Project }) => {
+  return (
+    <div className="flex flex-row p-4 my-8 h-50 bg-secondary border-primary-light border-1 gap-4 rounded-xl">
+      <ProjectImage imageSrc={project.imageSrc} />
+      <div className="flex flex-col w-xl gap-2">
+        <h2 className="text-2xl">{project.name}</h2>
+        <p className="flex-1 overflow-hidden text-ellipsis line-clamp-4">
+          {project.description}
+        </p>
+        <TagSection tags={project.tags} />
+      </div>
+    </div>
+  );
+};
+
+const ProjectImage = ({
+  imageSrc = [missingImage],
+}: {
+  imageSrc?: Array<string>;
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (imageSrc.length <= 1) return;
+
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % imageSrc.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [imageSrc]);
+
+  return (
+    <div className="aspect-square h-full w-auto">
+      <img
+        src={imageSrc[currentIndex]}
+        className="h-full w-full object-cover rounded-xl"
+      />
+    </div>
+  );
+};
+
+const TagSection = ({ tags }: { tags: Array<string> }) => {
+  return (
+    <div className="flex flex-row gap-2">
+      {tags.map((tag) => (
+        <TagContainer tag={tag} />
+      ))}
+    </div>
+  );
+};
+
+const TagContainer = ({
+  tag,
+  className,
+}: {
+  tag: string;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={`rounded-full py-0.5 px-2 text-sm bg-neutral-light text-neutral-dark ${className}`}
+    >
+      {tag}
+    </div>
+  );
+};
